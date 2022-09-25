1b0d4a3d532faacba7ddfe7d6cd613a219156316
Nikhil <nikhilshetye24@gmail.com>
2022-09-24 19:32:25 -0700

    exp use context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import './App.scss';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-import SiteHeader from './components/SiteHeader/SiteHeader';
-import RecipePage from './pages/RecipePage/RecipePage';
-import HomePage from './pages/HomePage/HomePage';
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SiteHeader from "./components/SiteHeader/SiteHeader";
+import RecipePage from "./pages/RecipePage/RecipePage";
+import HomePage from "./pages/HomePage/HomePage";
+import { useState, createContext } from "react";
+export const UserContext = createContext(null);
 
 function App() {
+  const userState = useState({
+    firstName: "James",
+    lastName: "Jameson",
+    suffix: 1,
+    email: "jamesjameson@example.com",
+  });
+
   return (
     <BrowserRouter>
-      <SiteHeader />
-      <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path="/:id" element={<RecipePage/>}></Route>
-      </Routes>
+      <UserContext.Provider value={userState}>
+        <SiteHeader />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/:id" element={<RecipePage />}></Route>
+        </Routes>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
--- src/pages/RecipePage/RecipePage.js
+++ src/pages/RecipePage/RecipePage.js
@@ -2,11 +2,13 @@ import { Col, Container, Row, } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import RecipeDetails from "../../components/RecipeDetails/RecipeDetails";
 import useGetRecipe from "../../hooks/useGetRecipe";
-
+import { useContext } from "react";
+import {UserContext} from '../../App'
 function RecipePage() {
   const params = useParams();
   const recipe = useGetRecipe(params.id);
-  
+  const [user, setUser] = useContext(UserContext);
+  console.log(user)
   if (!recipe) {
     return(
       <Container>
