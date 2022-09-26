ee1c157e7b9d4bd9c83f38987e2d093eee33434f
Nikhil <nikhilshetye24@gmail.com>
2022-09-25 23:11:27 -0700

    added profile page accordian

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -3,48 +3,112 @@ import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 import { useNavigate } from "react-router-dom";
-const {REACT_APP_API_SERVER_URL} = process.env;
+import Accordion from "react-bootstrap/Accordion";
+const { REACT_APP_API_SERVER_URL } = process.env;
 
 function Profile() {
   const [user, setUser] = useContext(UserContext);
   const [isLoading, setIsLoading] = useState(true);
-  const [userInfo, setUserInfo] = useState({});
-  const navigate = useNavigate()
-  async function getProfile(token){
-    try{
-      const {data} = await axios.get(`${REACT_APP_API_SERVER_URL}/users/profile`,{
-        headers: {
-          Authorization: `Bearer ${token}`,
+  const navigate = useNavigate();
+  const [userRecipeList, setUserRecipeList] = useState(null)
+  // function getFavRecipes(){
+  //   let promises = user.favRecipes.map((recipe)=>{
+  //     return axios.get(`${REACT_APP_API_SERVER_URL}/recipes/${recipe.id}`)
+  //   })
+  //   console.log(promises)
+  // }
+
+  async function getProfile(token) {
+    try {
+      const { data } = await axios.get(
+        `${REACT_APP_API_SERVER_URL}/users/profile`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-      })
-      console.log(data)
-      setIsLoading(false)
-      setUserInfo(data)
-      setUser(data)
-    }catch(err){
-      console.log(err)
-      navigate("/login")
+      );
+      console.log(data);
+      setIsLoading(false);
+      setUser(data);
+    } catch (err) {
+      console.log(err);
+      navigate("/login");
     }
   }
 
-  function handleLogOut(){
-    sessionStorage.removeItem("token")
-    setUser(null)
-    navigate("/")
+  function handleLogOut() {
+    sessionStorage.removeItem("token");
+    setUser(null);
+    navigate("/");
   }
   useEffect(() => {
-    const token = sessionStorage.getItem("token")
-    getProfile(token)
+    const token = sessionStorage.getItem("token");
+    getProfile(token);
     // Here grab the token from sessionStorage and then make an axios request to profileUrl endpoint.
     // Remember to include the token in Authorization header
   }, []);
 
-  return (
-    
-    isLoading ? <h1>Loading...</h1> : <>
-    <h1>Welcome {userInfo.name}!</h1>
-    <button type="button" onClick={handleLogOut}>Logout</button>
-    </>);
+  useEffect(() => {
+    if (user && !user.favRecipes.length) {
+        let promises = user.favRecipes.map((recipe) => {
+          return axios.get(`${REACT_APP_API_SERVER_URL}/recipes/${recipe.recipeId}`);
+        });
+
+      
+      async function getFavRecipes() {
+        try{
+          const data = await Promise.all(promises)
+          const recipeList = data.map((e)=>{
+            return e.data
+          })
+          console.log(recipeList)
+          setUserRecipeList(recipeList)
+        }catch(err){
+          console.log(err)
+        }
+      }
+      getFavRecipes()
+    }
+   
+  }, [user]);
+
+  return isLoading ? (
+    <div className="container">
+      <div className="row">
+        <h1>Loading...</h1>
+      </div>
+    </div>
+  ) : (
+    <div className="container">
+      <div className="row mb-3">
+        <div className="col-sm-3">
+          <h1 className="fs-4">Welcome {user.name}!</h1>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={handleLogOut}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-6">
+          {userRecipeList && <Accordion defaultActiveKey="0">
+            {userRecipeList.map((recipe) => {
+              return (
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>{`${recipe.title}`}</Accordion.Header>
+                  <Accordion.Body>{`${recipe.ingredient}`}</Accordion.Body>
+                </Accordion.Item>
+              );
+            })}
+          </Accordion>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
